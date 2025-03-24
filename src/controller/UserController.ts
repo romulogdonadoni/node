36bfbcd76723087a14267ed3e0b277a596f78dac
romulogdonadoni <romulogdonadoni@gmail.com>
2025-03-24 20:19:28 -0300

    update

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -9,6 +9,12 @@ export function listUser(req: Request, res: Response): void {
 
 export function getUser(req: Request, res: Response): void {
     if (!user) {
-
+        res.status(404).json({ error: "User not found" });
+    } else {
+        res.status(200).json(user);
     }
+}
+
+export function createUser(req: Request, res: Response): void {
+    onst { id, name, age } = req.body
 }
